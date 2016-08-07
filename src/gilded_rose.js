19a910367b42e0190c6f76ffc3444a403ad23531
Junius Sim <junius.sim@gmail.com>
2016-08-07 19:03:26 +0800

    After Refactor 3: Use a switch statement instead of a long list of If nots - this makes it easier to identify which codes after which category of items. Intially it was difficult to find out which code was affect which category. I would have to follow through the logic of the if not's. Because of that it was diffcult to refactor the some of the codes. Using the switch statement, it is clear which codes affect which items and this make refactoring much simpler

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -10,80 +10,75 @@ function Item(name, sell_in, quality) {
 // 3.Backstage passes
 // 4.Legendary item: Sulfuras
 
-// Rules
-// 1.Quality for all items except Sulfuras cannot be negative or more than 50
+/* Rules
+1. Quality for all items except Sulfuras cannot be negative or more than 50
+
+For Normal items
+- Quantity decreases 1:1
+- Once sell_in date is less than 0, quality decreases at double the speed
+- Quality cannot be negative (& cannot exceed 50, however no current tests test for that)
+
+For Aged Brie
+- Quality increases the older it gets 1:1
+- Quality cannot exceed 50
+- Quality also cannot be negative (so you cannot input negative value - the function does not handle it)
+
+For Backstage Passes
+- Quality increases the older it gets 1:1
+- Once sell_in date 10 days or less, quality increase by 1:2
+- Once sell_in date 5 days or less, quality increase by 1:3
+- Once sell_in date 0, quality decreases to 0
+
+For Sulfuras
+- Quality does not change
+- Sell_in date does not change
+
+*/
 
 // To get an overview, read the * comments
 
 function update_quality(items) {
   for (var i = 0; i < items.length; i++) {
-		// if item name is not Aged Brie & Item is not a Backstage Pass to a ... concert
-		// These items increases in quality as the sell_in date increases
-		// * This section reduces the quality of regular items
-    if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-			// quality of an item is never negative (we should throw an error if quality is negative instead of doing nothing)
-			if (items[i].quality > 0) {
-				// !with the exception of Sulfuras which quality never decreases
-        if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-          items[i].quality -= 1;
-        }
-      }
-    } else {
-			// * If it is either an Aged Brie or a Backstage pass
-			// This ensure that quality would not be increase further than 50, if its 50, we just leave it as 50
-      if (items[i].quality < 50) {
-				// * we would increase the quality
-        items[i].quality += 1;
-				// if it is a concert backstage pass, we would have additional conditions
-        if (items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-          if (items[i].sell_in < 11) {
-						// checking for quality not already being further than 50 before we increase it as quality could be greater than 50 after the first condition
-						// !we can probably do some refactoring here, like maybe just reduce quality by 2 or 3 depending on the range
+		switch (items[i].name) {
+			case "Sulfuras, Hand of Ragnaros":
+				break;
+			case "Aged Brie":
+				if (items[i].quality < 50) {
+					items[i].quality += 1;
+				}
+				items[i].sell_in -= 1;
+				break;
+			case "Backstage passes to a TAFKAL80ETC concert":
+				if (items[i].quality < 50) {
+					items[i].quality += 1;
+					if (items[i].sell_in < 11) {
 						if (items[i].quality < 50) {
-              items[i].quality += 1;
-            }
-          }
-          if (items[i].sell_in < 6) {
+							items[i].quality += 1;
+						}
+					}
+					if (items[i].sell_in < 6) {
             if (items[i].quality < 50) {
               items[i].quality += 1;
             }
           }
-        }
-      }
-    }
-		// If item is not sulfuras (the last category of item)
-		//* For all item categories except for sulfuras, we would reduce the sell_in date
-    if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-      items[i].sell_in -= 1;
-    } // Sulfuras sell_in date does not decrease then it shouldn't allowed to be negative
-
-
-		//* For all items categories with sell_in date less than 0
-    if (items[i].sell_in < 0) {
-			// Only Normal items, Backstage passes and Sulfuras
-      if (items[i].name != 'Aged Brie') {
-				// Only Normal Items and Sulfuras
-        if (items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-					// quality is above 0
+				}
+				items[i].sell_in -= 1;
+				if (items[i].sell_in < 0) {
+					items[i].quality = 0;
+				}
+				break;
+			default:
+				if (items[i].quality > 0) {
+					items[i].quality -= 1;
+				}
+				items[i].sell_in -= 1;
+				if (items[i].sell_in < 0) {
 					if (items[i].quality > 0) {
-						//* normal items whose quality is above 0
-            if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-              items[i].quality -= 1;
-            }
-						//* Do nothing for Sulfuras
-          }
-				//* When sell_in is negative, set Backstage Passes quality to zero
-        } else {
-          items[i].quality = 0;
-        }
-				//* when sell_in date for Aged brie is negative, as long as quality not above 0, increase the quality
-      } else {
-        if (items[i].quality < 50) {
-          items[i].quality += 1;
-        }
-      }
-    }
-  }
+						items[i].quality -= 1;
+					}
+				}
+		}
+	}
 }
 
 module.exports = {

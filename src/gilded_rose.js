2b5032b96dffd5b56e8dcddb7eb57efef7499222
Junius Sim <junius.sim@gmail.com>
2016-08-07 12:32:17 +0800

    Before Refactor 2

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -4,10 +4,6 @@ function Item(name, sell_in, quality) {
   this.quality = quality;
 }
 
-// Rf1: Made items a parameter that has to be passed into the update_quality function instead of having it as a global variable.
-// This reduce the global space pollution as well as allows us to apply the update_quality function to any array of inventory rather than just one array
-// var items = [];
-
 function update_quality(items) {
   for (var i = 0; i < items.length; i++) {
 		// if item name is not Aged Brie & Item is not a Backstage Pass to a ... concert
@@ -60,6 +56,5 @@ function update_quality(items) {
 
 module.exports = {
   Item: Item,
-	// items: items,
   update_quality: update_quality,
 };
